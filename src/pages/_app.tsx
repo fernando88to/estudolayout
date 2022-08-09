e01276f796d13d9cf0305c26e3f5cd5f83615ee5
fernandohenrique <fernandolima@tjto.jus.br>
2022-08-09 17:32:35 -0300

    configuração de thema claro

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,9 @@ import type {AppProps} from 'next/app'
 import ContextoGeral from "@/context/ContextoGeral";
 import {useState} from "react";
 import {Layout} from "@/components/Layout";
+import Head from "next/head";
+import {ThemeProvider} from "@mui/material";
+import {LighTheme} from "@/util/theme/LighTheme";
 
 
 function MyApp({Component, pageProps}: AppProps) {
@@ -11,9 +14,15 @@ function MyApp({Component, pageProps}: AppProps) {
     });
     return (
         <ContextoGeral.Provider value={{user, setUser}}>
-            <Layout>
-                <Component {...pageProps} />
-            </Layout>
+            <ThemeProvider theme={LighTheme}>
+                <Layout>
+                    <Head>
+                        <meta name="viewport" content="initial-scale=1, width=device-width" />
+                    </Head>
+                    <Component {...pageProps} />
+                </Layout>
+            </ThemeProvider>
+
         </ContextoGeral.Provider>
 
     );
